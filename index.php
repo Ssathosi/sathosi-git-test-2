460cd15147b665cca6f859d01e9910b8cec94052
Sathosi <sssathosi@gmail.com>
2024-05-20 21:38:20 +0700

    menambahkan paragraf

--- index.php
+++ index.php
@@ -7,5 +7,6 @@
 </head>
 <body>
     <H1>sathosi</H1>
+    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Porro omnis aperiam assumenda? Ullam minus dolorem ipsum dicta. Eum vitae dolor veniam voluptatum nam vero quod sunt unde animi, temporibus et?</p>
 </body>
 </html>
